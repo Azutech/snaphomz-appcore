99c8283db5d61b095c4a86b3ed8d41ee52e65744
Azutech <azunna.onugha@gmail.com>
2025-05-17 23:37:58 +0100

    Refactor email transport configuration to use dynamic settings from configs

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -55,14 +55,10 @@ export class EmailService {
       //   user: configs.EMAIL_USERNAME,
       //   pass: configs.EMAIL_PASSWORD,
       // },
-      // host: configs.EMAIL_HOST, // Should be `smtp-mail.outlook.com`
-      // port: parseInt(configs.EMAIL_PORT || '465'), // Use 587 (not 465)
-      // secure: true, // Required for port 587 (STARTTLS)
-      // requireTLS: true, // Enforce TLS encryption
-      host: 'smtp-mail.outlook.com',
-      port: 587,
-      secure: false, // Use false for STARTTLS on port 587
-      requireTLS: true,
+      host: configs.EMAIL_HOST, // Should be `smtp-mail.outlook.com`
+      port: parseInt(configs.EMAIL_PORT || '587'), // Use 587 (not 465)
+      secure: false, // Required for port 587 (STARTTLS)
+      requireTLS: true, // Enforce TLS encryption
       auth: {
         user: configs.EMAIL_USERNAME,
         pass: configs.EMAIL_PASSWORD,
